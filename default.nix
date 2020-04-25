9f01410562c8181556921798d5debb275d66959a
Jörg Thalheim <joerg@thalheim.io>
2020-04-25 17:17:35 +0100

    add -args & -build-args flags

--- default.nix
+++ default.nix
@@ -1,9 +1,11 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs;
-buildGoPackage {
+buildGoModule {
   pname = "nix-build-uncached";
   version = "0.0.0";
   src = ./.;
+
+  modSha256 = "1fl0wb1xj4v4whqm6ivzqjpac1iwpq7m12g37gr4fpgqp8kzi6cn";
 
   nativeBuildInputs = [ makeWrapper ];
 

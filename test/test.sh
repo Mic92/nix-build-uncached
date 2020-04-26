615c5610840d884b213bb8577f26a3177c698ae7
Jörg Thalheim <joerg@thalheim.io>
2020-04-26 11:05:21 +0100

    rename args/buildArgs to flags/buildFlags, use external nix file for filtering
    The latter change will work around large attribute sets that no longer
    fit into argv.

--- test/test.sh
+++ test/test.sh
@@ -6,9 +6,11 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 store=$(mktemp -p "$dir" -d)
 outputs=$(mktemp -p "$dir" -d)
 trap "{ chmod -R +w $store; rm -rf $store $outputs; }" EXIT
-go run $dir/.. -args "--store '$store'" -build-args "-o '$outputs/result'" $dir/test.nix
+
+go run $dir/.. -flags "--store '$store'" -build-flags "-o '$outputs/result1'" $dir/test.nix
+go run $dir/.. -flags "--store '$store'" -build-flags "-o '$outputs/result2'" $dir/test2.nix
 count=$(find "$outputs" -type l | wc -l)
 
-if [[ $count != "1" ]]; then
-    echo "expect one package to be built, got $count"
+if [[ $count != "2" ]]; then
+    echo "expect two package to be built, got $count"
 fi

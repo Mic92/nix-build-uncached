e95d6b6ea19f1f7afd0bf4ee1cf26dffc310026b
Jörg Thalheim <joerg@thalheim.io>
2020-07-19 08:55:40 +0100

    fix release-notes script

--- scripts/print-release-notes.sh
+++ scripts/print-release-notes.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-set -eux -o pipefail
+set -eu -o pipefail
 
 escape() { perl -p -e 's/%/%25/;s/\r/%0D/;s/\n/%0A/'; }
-ref=${{ github.ref }}
+ref="$1"
 tag_name=${ref#refs/tags/}
 release_name=$(git tag -l --format='%(subject)' "$tag_name"  | escape)
 body=$(git tag -l --format='%(contents:body)' "$tag_name" | escape)
